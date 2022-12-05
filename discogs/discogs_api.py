5fe2a86790f12d1a137c89a459a730c44d9a5e19
Pedro Salvado <ppsalvado@gmail.com>
2022-12-05 13:33:29 +0000

    another indices commit

--- discogs/discogs_api.py
+++ discogs/discogs_api.py
@@ -1,31 +1,41 @@
 import discogs_client
 import requests
+from soundtrack-frontend.indices import find_info_in_data
+
+film, image_names = find_info_in_data(indices)
 
 
 """Runs a query on the Discogs API for the film name to return full title,
 year and genre of the same film"""
 
 def find_ost(film):
+    full_titles = []
+    year_releases =[]
+    genres = []
+    for i in range(len(indices)):
+        film_title = film[i][0]
+        year_release = film[i][1]
+        url = f'https://api.discogs.com/database/search?q={film_title/'
 
-    url = f'https://api.discogs.com/database/search?q={film/'
+        params = {'key':consumer_key,
+                'secret':consumer_secret,
+                'type':'release',
+                'style':'soundtrack',
+                'format':'CD',
+                'year': year_release
+                }
+        response = requests.get(url,params=params)
 
-    params = {'key':consumer_key,
-             'secret':consumer_secret,
-             'type':'release',
-             'style':'soundtrack',
-             'format':'CD'
-             }
-    response = requests.get(url,params=params)
+        if response.status_code != 200:
+            return ''
 
-    if response.status_code != 200:
-        return ''
+        data = response.json()
 
-    data = response.json()
-
-    full_title = data['results'][0]['title']
-
-    year = data['results'][0]['year']
-
-    genre = data['results'][0]['genre']
-
-    return full_title, year, genre
+        full_title = data['results'][0]['title']
+        full_titles.append(full_title)
+        year = data['results'][0]['year']
+        year_releases.append(year)
+        genre = data['results'][0]['genre']
+        genres.append(genre)
+        
+    return full_titles, year_releases, genres
